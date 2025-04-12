16f06f7973da9b0e2478cc5c5676b21f4a985a76
Giorgi Tsintsadze <cincadze880@gmail.com>
2025-04-12 12:03:02 +0400

    move into different dir

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Game implements Runnable {
+    public void run() {
+        SwingUtilities.invokeLater(new StartMenu());
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Game());
+    }
+}
