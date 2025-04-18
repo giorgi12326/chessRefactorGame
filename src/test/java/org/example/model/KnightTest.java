75a082b1146d0c3d6b814bd1f6205b8c41aeb985
Giorgi Tsintsadze <cincadze880@gmail.com>
2025-04-18 23:42:04 +0400

    add knight test

---
+++ src/test/java/org/example/model/KnightTest.java
@@ -0,0 +1,61 @@
+package org.example.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.example.model.Board.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class KnightTest {
+    Board board;
+    private King blackKing;
+    private King whiteKing;
+    private CheckmateDetector checkmateDetector;
+    private Knight knight;
+
+    @BeforeEach
+    public void setup(){
+        board = new Board(null,null);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                board.getSquareArray()[i][j].liftUpThePiece();
+            }
+        }
+
+        knight = new Knight(1, board.getSquareArray()[2][2], RESOURCES_WQUEEN_PNG);
+        board.getSquareArray()[2][2].put(knight);
+
+        whiteKing = new King(1, board.getSquareArray()[7][7], RESOURCES_WKING_PNG,board);
+        board.getSquareArray()[7][7].put(whiteKing);
+
+        blackKing = new King(0, board.getSquareArray()[0][3], RESOURCES_BKING_PNG,board);
+        board.getSquareArray()[0][3].put(blackKing);
+
+        LinkedList<Piece> wPieces = new LinkedList<>();
+        LinkedList<Piece> bPieces = new LinkedList<>();
+        bPieces.add(board.getSquareArray()[0][3].getOccupyingPiece());
+        wPieces.add(board.getSquareArray()[7][7].getOccupyingPiece());
+        wPieces.add(board.getSquareArray()[2][2].getOccupyingPiece());
+
+        checkmateDetector = new CheckmateDetector(board,wPieces,bPieces,whiteKing,blackKing);
+    }
+    @Test
+    public void TestLegalMoves(){
+        List<Square> legalMoves = knight.getLegalMoves(board);
+        List<Square> squares = List.of(
+                board.getSquareArray()[4][1],
+                board.getSquareArray()[4][3],
+                board.getSquareArray()[0][1],
+                board.getSquareArray()[0][3],
+                board.getSquareArray()[1][4],
+                board.getSquareArray()[3][4],
+                board.getSquareArray()[1][0],
+                board.getSquareArray()[3][0]
+        );
+        assertTrue(squares.containsAll(legalMoves));
+        assertTrue(legalMoves.containsAll(squares));
+    }
+}
